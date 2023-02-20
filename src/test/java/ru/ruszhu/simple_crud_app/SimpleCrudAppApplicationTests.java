5ca83c7c54deda2786fd5a3e56115790cba8c91b
rus-zhu <microlab982@yandex.ru>
2023-02-20 12:59:55 +0500

    init

---
+++ src/test/java/ru/ruszhu/simple_crud_app/SimpleCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ruszhu.simple_crud_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
